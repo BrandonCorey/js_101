4bc595d2d2bb5d92b003e6083768acf2b50997c1
Brandon Corey <bcorey3660@gmail.com>
2022-08-13 17:37:47 -0400

    Refactors code for RPSLSP game

--- lesson_2_small_programs/rock_paper_scissors.js
+++ lesson_2_small_programs/rock_paper_scissors.js
@@ -1,18 +1,18 @@
-
 const readline = require('readline-sync');
 const PROMPT_MESSAGES = require('./rps_message.json');
 const VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard'];
 const winCounter = createWinCounter();
 
-// Main program gets input and returns winner
+// Main program gets input and calls functions
 while (true) {
+
   displayWinCounter(winCounter);
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-  console.log(PROMPT_MESSAGES['abbrev']);
+  prompt(PROMPT_MESSAGES['chooseOne']);
+  console.log(PROMPT_MESSAGES['abbreviatons']);
 
   let humanChoice = readline.question().toLowerCase();
   humanChoice = checkAbbreviation(humanChoice);
-  humanChoice = getValidRPS(humanChoice);
+  humanChoice = checkValidChoice(humanChoice);
 
   let cpuChoiceIndex = getRandomIndex();
   let cpuChoice = VALID_CHOICES[cpuChoiceIndex];
@@ -29,22 +29,28 @@ while (true) {
   }
 
   prompt(PROMPT_MESSAGES["playAgain"]);
-  let answer = readline.question().toLowerCase();
+  let response = readline.question().toLowerCase();
 
-  while (!(answer[0] === 'n' || answer === 'y')) {
-    prompt(PROMPT_MESSAGES["errorYorN"]);
-    answer = readline.question().toLowerCase();
-  }
+  getValidPlayAgain(response);
 
-  if (answer[0] !== 'y') break;
+  if (response[0] !== 'y') break;
 }
 
-// Caclulates the winner and displays on screen
-function dipslayWinner (winner) {
-  prompt(winner);
+// Creates win couinter for CPU and human
+function createWinCounter() {
+  let counterObj = {
+    cpu: 0,
+    human: 0
+  };
+  return counterObj;
 }
 
-// Converts abbreviated inputs to full name for logic
+function displayWinCounter (counter) {
+  prompt(`Cpu wins: ${counter.cpu}`);
+  prompt(`Your wins: ${counter.human}\n`);
+}
+
+// Converts abbreviated inputs to full name
 function checkAbbreviation(choice) {
   switch (choice) {
     case 'r':
@@ -61,14 +67,27 @@ function checkAbbreviation(choice) {
   return choice;
 }
 
-// Adds an arrow to CLI prompt
-function prompt(message) {
-  console.log(`=> ${message}`);
+// Validates human RPSLSP choice
+function checkValidChoice (choice) {
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt(PROMPT_MESSAGES["error"]);
+    choice = readline.question();
+    choice = checkAbbreviation(choice.toLowerCase());
+  }
+  return choice;
 }
 
+// Gets random index for CPU choice
+function getRandomIndex() {
+  let index = Math.floor(Math.random() * VALID_CHOICES.length);
+  return index;
+}
+
+// Calculates winner based off index differentials where CPU wins
+// Uses the indeces of the VALID_CHOICES array
 function calcWinner(cpuIndex, userChoice) {
   let userChoiceIndex = VALID_CHOICES.indexOf(userChoice);
-  const cpuWinCondition = [1, 3, -2, -4]; // --> All index differentials where computer wins
+  const cpuWinCondition = [1, 3, -2, -4];
   const cpuWinFormula = cpuIndex - userChoiceIndex;
 
   if (cpuWinCondition.includes(cpuWinFormula)) {
@@ -82,46 +101,41 @@ function calcWinner(cpuIndex, userChoice) {
 
 }
 
-
-function createWinCounter() {
-  let counterObj = {
-    cpu: 0,
-    human: 0
-  };
-  return counterObj;
-}
+// Updates the win counter each round
 function updateWinCounter(roundWinner, counter) {
-
   if (roundWinner === 'Computer wins!') counter.cpu += 1;
   if (roundWinner === 'You win!') counter.human += 1;
   return winCounter;
 }
 
-function displayWinCounter (counter) {
-  prompt(`Cpu wins: ${counter.cpu}`);
-  prompt(`Your wins: ${counter.human}\n`);
+// Prints winner on screen
+function dipslayWinner (winner) {
+  prompt(winner);
 }
 
+// Calculates the grand winner
 function calcGrandWinner (counter) {
   if (counter.cpu === 5) return PROMPT_MESSAGES['grandWinnerCpu'];
   if (counter.human === 5) return PROMPT_MESSAGES['grandWinnerHuman'];
   return 0;
 }
 
+// Prints grand winner to the screen
 function displayGrandWinner(grandWinner) {
   prompt(grandWinner);
 }
 
-function getValidRPS (choice) {
-  while (!VALID_CHOICES.includes(choice)) {
-    prompt(PROMPT_MESSAGES["error"]);
-    choice = readline.question();
-    choice = checkAbbreviation(choice);
+// Validates play again input
+function getValidPlayAgain(response) {
+  while (!(response[0] === 'n' || response === 'y')) {
+    prompt(PROMPT_MESSAGES["errorYorN"]);
+    response = readline.question();
+    response.toLowerCase();
   }
-  return choice;
+  return response;
 }
 
-function getRandomIndex() {
-  let index = Math.floor(Math.random() * VALID_CHOICES.length);
-  return index;
+// Adds an arrow to CLI prompt
+function prompt(message) {
+  console.log(`=> ${message}`);
 }
