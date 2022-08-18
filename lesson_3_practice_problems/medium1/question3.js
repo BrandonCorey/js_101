3a486e49f5acf5e137517ed3331385681eaa7fa4
Brandon Corey <bcorey3660@gmail.com>
2022-08-17 21:42:59 -0400

    Adds question3 to medium1

---
+++ lesson_3_practice_problems/medium1/question3.js
@@ -0,0 +1,15 @@
+//. Refactor code so it doesn't bust when entering 0 or negative number
+
+function factors(number) {
+  let divisor = number;
+  let factors = [];
+  while (divisor > 0) {
+    if (number % divisor === 0) {
+      factors.push(number / divisor);
+    }
+    divisor -= 1;
+  }
+  return factors;
+}
+
+console.log(factors(-5));
