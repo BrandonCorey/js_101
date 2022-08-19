23c0a7377dd237c3dea95a1498327c67c167b5d0
Brandon Corey <bcorey3660@gmail.com>
2022-08-19 00:26:08 -0400

    Refactors searching101

--- small_problems_exercises/easy4/searching101.js
+++ small_problems_exercises/easy4/searching101.js
@@ -2,18 +2,17 @@ const readline = require('readline-sync');
 const numOrder = ['1st', '2nd', '3rd', '4th', '5th'];
 const numArr = [];
 
-let lastNum = getNums();
-includesLastNum(lastNum);
+getNums();
+let getLastNum = () => readline.question(`Enter the last number: `);
+includesLastNum(getLastNum());
 
 function getNums() {
   for (let place of numOrder) {
     numArr.push(readline.question(`Enter the ${place} number: `));
   }
-  let last = readline.question(`Enter the last numner: `);
-  return last;
 }
 
-function includesLastNum() {
+function includesLastNum(lastNum) {
   let values = numArr;
   if (values.includes(lastNum)) {
     console.log(`The number ${lastNum} appears in ${values}.`);
