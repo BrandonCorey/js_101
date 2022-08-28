b6768661c75b3225381e934c349abab162a99ab8
Brandon Corey <bcorey3660@gmail.com>
2022-08-28 16:31:04 -0400

    Changes to easy 5

---
+++ small_problems_exercises/easy5/double_char.js
@@ -0,0 +1,13 @@
+// Write a function that takes a string,
+// doubles every character in the string,
+// and returns the result as a new string.
+function repeater(string) {
+  return string.split('').reduce((previous, current) => {
+    return previous + current.repeat(2);
+  }, '');
+}
+//console.log(repeater('hello'));
+// Test cases
+console.log(repeater('Hello'));        // "HHeelllloo"
+console.log(repeater('Good job!'));    // "GGoooodd  jjoobb!!"
+console.log(repeater(''));             // ""
