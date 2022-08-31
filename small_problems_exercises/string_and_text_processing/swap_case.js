c95e1f6c02eaae4932503faa07005bdcd92d254a
Brandon Corey <bcorey3660@gmail.com>
2022-08-30 23:17:55 -0400

    Adds swap case to string processing for small problems

---
+++ small_problems_exercises/string_and_text_processing/swap_case.js
@@ -0,0 +1,13 @@
+// Write a function that takes a string as an argument
+// and returns that string with every lowercase letter
+// changed to uppercase and every uppercase letter changed to lowercase.
+// Leave all other characters unchanged.
+
+function swapCase(string) {
+  return [...string].map(char => {
+    return char.toUpperCase() === char ? char.toLowerCase() : char.toUpperCase();
+  }).join('');
+}
+
+console.log(swapCase('CamelCase'));              // "cAMELcASE"
+console.log(swapCase('Tonight on XYZ-TV'));      // "tONIGHT ON xyz-tv"
