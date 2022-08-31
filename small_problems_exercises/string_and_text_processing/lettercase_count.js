bd91406e94dc9f5210a25d49793f2508ad1e5f5b
Brandon Corey <bcorey3660@gmail.com>
2022-08-30 22:12:07 -0400

    Adds lettercase count to string processing for small programs

---
+++ small_problems_exercises/string_and_text_processing/lettercase_count.js
@@ -0,0 +1,15 @@
+function letterCaseCount(string) {
+  const count = {lowercase: 0, uppercase: 0, neither: 0};
+
+  [...string].forEach(char => {
+    if (/[a-z]/g.test(char)) count.lowercase += 1;
+    if (/[A-Z]/g.test(char)) count.uppercase += 1;
+    if (/[^a-z]/gi.test(char)) count.neither += 1;
+  });
+  return count;
+}
+
+console.log(letterCaseCount('abCdef 123'));  // { lowercase: 5, uppercase: 1, neither: 4 }
+console.log(letterCaseCount('AbCd +Ef'));    // { lowercase: 3, uppercase: 3, neither: 2 }
+console.log(letterCaseCount('123'));         // { lowercase: 0, uppercase: 0, neither: 3 }
+console.log(letterCaseCount(''));            // { lowercase: 0, uppercase: 0, neither: 0 }
