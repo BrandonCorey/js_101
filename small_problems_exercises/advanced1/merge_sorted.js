87139ed92026cd80b8583be0a788fa506680b9db
Brandon Corey <bcorey3660@gmail.com>
2022-09-25 23:14:53 -0400

    Adds merge_sort.js

--- small_problems_exercises/advanced1/merge_sorted.js
+++ small_problems_exercises/advanced1/merge_sorted.js
@@ -46,7 +46,6 @@ console.log(merge([1, 5, 9], [2, 6, 8]));      // [1, 2, 5, 6, 8, 9]
 console.log(merge([1, 1, 3], [2, 2]));         // [1, 1, 2, 2, 3]
 console.log(merge([], [1, 4, 5]));             // [1, 4, 5]
 console.log(merge([1, 4, 5], []));             // [1, 4, 5]
-
 // Loop through first array in outer loop
 // Loop through subsequent array in inner loop
 // At first index, find the lower value
